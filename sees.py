fa946e3c5c4f89ef8eef3b420db04275da9d8620
Gökhan ALKAN <cigalkan@gmail.com>
2014-10-12 20:43:31 +0300

    Update sees.py

--- sees.py
+++ sees.py
@@ -18,25 +18,33 @@ def signal_handler(signal, frame):
 
 
 def sees():
-	main = Main()
+
 	signal.signal(signal.SIGINT, signal_handler)	
 
+	main = Main()
 	while True:
 		mess = bcolors.FAIL + disclamer + bcolors.ENDC + " : "
 		sys.stdout.write(mess)
 
-		agree = raw_input()
-		if (agree == "Y" or agree == "y"):
-			try:	
-				main.run()
-			except Exception, err:
-				print >> sys.stderr, err
+		if not main.args.warning:
+			agree = raw_input()
+			if (agree == "Y" or agree == "y"):
+				try:	
+					main.run()
+				except Exception, err:
+					print >> sys.stderr, err
+					sys.exit(1)
+			elif (agree == "N" or agree == "n"):
+				print >> sys.stderr, message
 				sys.exit(1)
-		elif (agree == "N" or agree == "n"):
-			print >> sys.stderr, message
-			sys.exit(1)
+			else:
+				print >> sys.stderr, wrong_option
 		else:
-			print >> sys.stderr, wrong_option
+			try:
+                        	main.run()
+                        except Exception, err:
+                               	print >> sys.stderr, err
+                                sys.exit(1)
 
 
 if __name__ == "__main__":
@@ -45,3 +53,4 @@ if __name__ == "__main__":
 	"""
 
 	sees()
+
